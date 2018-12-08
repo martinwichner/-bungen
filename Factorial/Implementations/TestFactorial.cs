9063cd904560ecc2d6afb011a36e9433ccc91d8a
Martin Wichner <martin.wichner@googlemail.com>
2018-12-08 18:01:14 +0100

    Add test implementation for factorial

---
+++ Factorial/Implementations/TestFactorial.cs
@@ -0,0 +1,45 @@
+﻿namespace Factorial.Implementations
+{
+    public class TestFactorial : IFactorial
+    {
+
+        public TestFactorial(bool useRecursion)
+        {
+            
+        }
+        /// <summary>
+        /// <para>Factorial multiplies all numbers from 1 to n.</para>
+        /// <para>Factorial(0) = 1</para>
+        /// <para>Factorial(n) = 1 * 2 * 3 * ...* n-1 * n</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int Factorial(int n)
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// <para>UnevenFactorial multiplies all uneven numbers from 1 to n.</para>
+        /// <para>Factorial(8) = 1 * 3 * 5 * 7</para>
+        /// <para>Factorial(9) = 1 * 3 * 5 * 7 * 9</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int UnevenFactorial(int n)
+        {
+            return 1;
+        }
+
+        /// <summary>
+        /// <para>SquareFactorial multiplies all squares from 1 to n.</para>
+        /// <para>Factorial(n) = 1^2 * 2^2 * 3^2 * ... * (n-1)^2 * n^2</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int SquareFactorial(int n)
+        {
+            return 1;
+        }
+    }
+}
---
+++ FactorialTests/TestFactorial.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactorialTests
+{
+    [TestClass]
+    public class TestFactorial
+    {
+        [DataRow(1, 1, true)]
+        [DataRow(1, 1, false)]
+        [DataRow(1, 1, true)]
+        [DataRow(-1, 1, false)]
+        [DataRow(7, 5040, true)]
+        [DataRow(7, 5040, false)]
+        [DataTestMethod]
+        public void Factorial(int n, int expected, bool useRecursion)
+        {
+            var factorial = new Factorial.Implementations.TestFactorial(useRecursion);
+            var result = factorial.Factorial(n);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataRow(1, 1, true)]
+        [DataRow(1, 1, false)]
+        [DataRow(1, 1, true)]
+        [DataRow(-1, 1, false)]
+        [DataRow(7, 105, true)]
+        [DataRow(7, 105, false)]
+        [DataRow(8, 105, true)]
+        [DataRow(8, 105, false)]
+        [DataTestMethod]
+        public void UnevenFactorial(int n, int expected, bool useRecursion)
+        {
+            var factorial = new Factorial.Implementations.TestFactorial(useRecursion);
+            var result = factorial.UnevenFactorial(n);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataRow(1, 1, true)]
+        [DataRow(1, 1, false)]
+        [DataRow(1, 1, true)]
+        [DataRow(-1, 1, false)]
+        [DataRow(7, 25401600, true)]
+        [DataRow(7, 25401600, false)]
+        [DataTestMethod]
+        public void SquareFactorial(int n, int expected, bool useRecursion)
+        {
+            var factorial = new Factorial.Implementations.TestFactorial(useRecursion);
+            var result = factorial.SquareFactorial(n);
+            Assert.AreEqual(expected, result);
+        }
+
+    }
+}
