79a472d52266c72866fd72c06d28d60eaec0a659
Martin Wichner <martin.wichner@googlemail.com>
2018-12-08 17:39:51 +0100

    Add Factorial

---
+++ Factorial/IFactorial.cs
@@ -0,0 +1,29 @@
+﻿namespace Factorial
+{
+    public interface IFactorial
+    {
+        /// <summary>
+        /// <para>Factorial multiplies all numbers from 1 to n.</para>
+        ///  <para>Factorial(0) = 1</para>
+        ///  <para>Factorial(n) = 1 * 2 * 3 * ...* n-1 * n</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        int Factorial(int n);
+        /// <summary>
+        /// <para>UnevenFactorial multiplies all uneven numbers from 1 to n.</para>
+        ///  <para>Factorial(8) = 1 * 3 * 5 * 7</para>
+        ///  <para>Factorial(9) = 1 * 3 * 5 * 7 * 9</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        int UnevenFactorial(int n);
+        /// <summary>
+        /// <para>SquareFactorial multiplies all squares from 1 to n.</para>
+        ///  <para>Factorial(n) = 1^2 * 2^2 * 3^2 * ... * (n-1)^2 * n^2</para>
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        int SquareFactorial(int n);
+    }
+}
---
+++ Factorial/Implementations/ExampleFactorial.cs
@@ -0,0 +1,99 @@
+﻿using System.Linq;
+
+namespace Factorial.Implementations
+{
+    public class ExampleFactorial : IFactorial
+    {
+        public bool UseRecursion { get; set; }
+
+        public ExampleFactorial(bool useRecursion)
+        {
+            UseRecursion = useRecursion;
+        }
+
+        public int Factorial(int n)
+        {
+            if (n < 1)
+            {
+                return 1;
+            }
+
+            return UseRecursion ? FactorialWithRecursion(n) : FactorialWithoutRecursion(n);
+        }
+
+        private static int FactorialWithRecursion(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * FactorialWithRecursion(n - 1);
+            }
+        }
+
+        private static int FactorialWithoutRecursion(int n)
+        {
+            return Enumerable.Range(1, n).Aggregate((current, next) => current * next);
+        }
+
+        public int UnevenFactorial(int n)
+        {
+            if (n < 1)
+            {
+                return 1;
+            }
+
+            return UseRecursion ? UnevenFactorialWithRecursion(n) : UnevenFactorialWithoutRecursion(n);
+        }
+
+        private static int UnevenFactorialWithRecursion(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (n % 2 == 0)
+                {
+                    n -= 1;
+                }
+                return n * UnevenFactorialWithRecursion(n - 2);
+            }
+        }
+
+        private static int UnevenFactorialWithoutRecursion(int n)
+        {
+            return Enumerable.Range(1, n).Where(c => (c % 2 != 0)).Aggregate((current, next) => current * next);
+        }
+
+        public int SquareFactorial(int n)
+        {
+            if (n < 1)
+            {
+                return 1;
+            }
+
+            return UseRecursion ? SquareFactorialWithRecursion(n) : SquareFactorialWithoutRecursion(n);
+        }
+
+        private static int SquareFactorialWithRecursion(int n)
+        {
+            if (n <= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (n * n) * SquareFactorialWithRecursion(n - 1);
+            }
+        }
+
+        private static int SquareFactorialWithoutRecursion(int n)
+        {
+            return Enumerable.Range(1, n).Aggregate((current, next) => current * (next * next));
+        }
+    }
+}
