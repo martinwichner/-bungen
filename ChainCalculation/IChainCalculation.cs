77a62bd0fa8046b7e97900585bf9153e240084be
Martin Wichner <martin.wichner@googlemail.com>
2018-12-08 20:15:10 +0100

    Add ChainCalculation interface and example implementation

---
+++ ChainCalculation/IChainCalculation.cs
@@ -0,0 +1,16 @@
+﻿namespace ChainCalculation
+{
+    public interface IChainCalculation
+    {
+        /// <summary>
+        /// Calculate input. Example input: "3 * 4 + 6 - 1"
+        /// Start and end with a number.
+        /// Between 2 numbers is one operator (+, -, *, /)
+        /// Between one number and one operator is exactly one whitespace
+        /// Calculation is from left to right
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        int Calculate(string input);
+    }
+}
---
+++ ChainCalculation/Implementations/ExampleChainCalculation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ChainCalculation.Implementations
+{
+    public class ExampleChainCalculation : IChainCalculation
+    {
+        public int Calculate(string input)
+        {
+            if (IsValid(input))
+            {
+                var splittedInput = input.Split(' ');
+                var result = int.Parse(splittedInput[0]);
+                for (int i = 1; i < splittedInput.Length; i += 2)
+                {
+                    switch (splittedInput[i])
+                    {
+                        case "+":
+                            result += int.Parse(splittedInput[i + 1]);
+                            break;
+                        case "-":
+                            result -= int.Parse(splittedInput[i + 1]);
+                            break;
+                        case "*":
+                            result *= int.Parse(splittedInput[i + 1]);
+                            break;
+                        case "/":
+                            result /= int.Parse(splittedInput[i + 1]);
+                            break;
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                return int.MinValue;
+            }
+        }
+
+        private static bool IsValid(string input)
+        {
+            return Regex.IsMatch(input, @"^\d+( [+\-*/] \d+)*$");
+        }
+    }
+}
