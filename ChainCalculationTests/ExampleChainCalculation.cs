4dfc7f161e59585c574d5aca766afcf487ca0b60
Martin Wichner <martin.wichner@googlemail.com>
2018-12-09 11:25:46 +0100

    Add tests

---
+++ ChainCalculationTests/ExampleChainCalculation.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChainCalculationTests
+{
+    [TestClass]
+    public class ExampleChainCalculation
+    {
+        [DataRow("3 * 3 * 3 * 3", 81)]
+        [DataRow("3  ",int.MinValue)]
+        [DataTestMethod]
+        public void ChainCalculation(string input, int expected)
+        {
+            var chainCalcuator = new ChainCalculation.Implementations.ExampleChainCalculation();
+            var result = chainCalcuator.Calculate(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
