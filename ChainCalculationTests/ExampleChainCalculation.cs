81e814278453a2f6273443706bef667e61e3de85
Martin Wichner <martin.wichner@googlemail.com>
2018-12-09 11:32:02 +0100

    Add new tests data rows

--- ChainCalculationTests/ExampleChainCalculation.cs
+++ ChainCalculationTests/ExampleChainCalculation.cs
@@ -7,6 +7,7 @@ namespace ChainCalculationTests
     {
         [DataRow("3 * 3 * 3 * 3", 81)]
         [DataRow("3  ",int.MinValue)]
+        [DataRow("1 + 2 * 3 - 4 / 2", 2)]
         [DataTestMethod]
         public void ChainCalculation(string input, int expected)
         {
